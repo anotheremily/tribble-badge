a1cce149dc2326d05144f8707d38416132aebe00
Emily Young <emily@zavtralabs.com>
2017-05-27 00:48:13 -0400

    initial commit of eeprom code

---
+++ src/bb01/store.cpp
@@ -0,0 +1,53 @@
+#include "store.h"
+
+void storeInit(uint8_t badgeId) {
+    EEPROM.write(ADDRESS_BADGE_ID, badgeId);
+    EEPROM.write(ADDRESS_LEVEL, 0);
+    EEPROM.write(ADDRESS_PAIRING_COUNT, 0);
+
+    for (uint8_t i = 0; i < UNLOCK_FEATURES; i += 1) {
+        EEPROM.write(ADDRESS_UNLOCK_START + i, 0);
+    }
+
+    for (uint8_t i = 0; i < NUM_BADGES; i += 1) {
+        EEPROM.write(ADDRESS_PAIRING_START + i, 0);
+    }
+}
+
+uint8_t getBadgeId() {
+    return EEPROM.read(ADDRESS_BADGE_ID);
+}
+
+uint8_t getLevel() {
+    return EEPROM.read(ADDRESS_BADGE_ID);
+}
+
+uint8_t incrLevel() {
+    uint8_t level = EEPROM.read(ADDRESS_LEVEL) + 1;
+    EEPROM.write(ADDRESS_LEVEL, level);
+    return level;
+}
+
+uint8_t incrPairingCount() {
+    uint8_t level = EEPROM.read(ADDRESS_PAIRING_COUNT) + 1;
+    EEPROM.write(ADDRESS_PAIRING_COUNT, level);
+    return level;
+}
+
+bool isUnlocked(uint8_t featureId) {
+    return featureId < UNLOCK_FEATURES &&
+        EEPROM.read(ADDRESS_UNLOCK_START + featureId) ? true : false;
+}
+
+bool isPaired(uint8_t extBadgeId) {
+    return extBadgeId < NUM_BADGES &&
+        EEPROM.read(ADDRESS_PAIRING_START + extBadgeId) ? true : false;
+}
+
+bool setPaired(uint8_t extBadgeId) {
+    if (extBadgeId >= NUM_BADGES) {
+        return false;
+    }
+    EEPROM.write(ADDRESS_PAIRING_START + extBadgeId, 1);
+    return true;
+}
---
+++ src/bb01/store.h
@@ -0,0 +1,74 @@
+/**
+ * EEPROM storage
+ * Emily Young <emily@zavtralabs.com>
+ *
+ * From Arduino site:
+ * An EEPROM write takes 3.3 ms to complete. The EEPROM memory has a
+ * specified life of 100,000 write/erase cycles, so you may need to
+ * be careful about how often you write to it.
+ */
+
+#ifndef STORE_H
+#define STORE_H
+
+#include <EEPROM.h>
+#include "constants.h"
+
+/**
+ * Initialize EEPROM.
+ * @param uint8_t badge id
+ */
+void storeInit(uint8_t);
+
+/**
+ * Get id of the badge
+ * @return badge id
+ */
+uint8_t getBadgeId();
+
+/**
+ * Get id of the level
+ * @return level
+ */
+uint8_t getLevel();
+
+/**
+ * Increase level by 1
+ * @return level
+ */
+uint8_t incrLevel();
+
+/**
+ * Get pairing count of the badge
+ * @return pairing count
+ */
+uint8_t getPairingCount();
+
+/**
+ * Increase pairing count by 1
+ * @return pairing count
+ */
+uint8_t incrPairingCount();
+
+/**
+ * Check if feature i is unlocked
+ * @param uint8_t feature id
+ * @return bool is unlocked
+ */
+bool isUnlocked(uint8_t);
+
+/**
+ * Check if badge is paired with connected badge
+ * @param uint8_t ext badge id
+ * @return bool is paired
+ */
+bool isPaired(uint8_t);
+
+/**
+ * Sets pairing
+ * @param uint8_t ext badge id
+ * @return bool is paired
+ */
+bool setPaired(uint8_t);
+
+#endif
